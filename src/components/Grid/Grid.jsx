6c82c94ff1520e297966ab9828ed32d2d86e81df
aashishrana <aashishrana71135@gmail.com>
2023-08-11 00:07:23 -0700

    Grid component is some more functionality added

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import Card from "../Card/Card";
-import "./Grid.css"
+import "./Grid.css";
 
-function Grid({numberOfCard}) {
-    const [board , setBoard] =  useState(Array(numberOfCard).fill(""));
-
-    return (
-        <div className="grid" >
-            {board.map((el , idx) => <Card key={idx} />)}
-        </div>
-    )
-
+function Grid({ numberOfCard }) {
+  const [board, setBoard] = useState(Array(numberOfCard).fill(""));
+  const [turn, setTurn] = useState(true);  // true => 0, false => X
+  return (
+    <div className="grid-wrapper">
+      <h1 className="grid-highlight"> Current turn : {(turn ? "O" : "X")}</h1>
+      <div className="grid">
+        {board.map((el, idx) => (
+          <Card key={idx} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Grid;
