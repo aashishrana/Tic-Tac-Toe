97156b6da0752fa95f39c7638231dd64b23f68bb
aashishrana <aashishrana71135@gmail.com>
2023-08-11 10:27:24 -0700

    Something wrong is happening on click value not changed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import Icon from "../Icon/Icon";
 import "./Card.css"
 
-function Card({player}) {
+function Card({player, onPlay, index}) {
   let icon = <Icon />;
 
   if (player == "X") {
@@ -10,7 +10,7 @@ function Card({player}) {
     icon = <Icon name="circle" />;
   }
 
-  return <div className="card">
+  return <div className="card" onClick={() => onPlay(index)}>
     {icon }
   </div>;
 }
--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -4,13 +4,37 @@ import "./Grid.css";
 
 function Grid({ numberOfCard }) {
   const [board, setBoard] = useState(Array(numberOfCard).fill(""));
-  const [turn, setTurn] = useState(true);  // true => 0, false => X
+  const [turn, setTurn] = useState(true); // true => 0, false => X
+  const [winner, setWinner] = useState(null);
+  function play(index) {
+    if (turn == true) {
+      board[index] = "O";
+    } else {
+      board[index] = "X";
+    }
+
+    const win = isWinner(board, turn ? "O" : "X");
+
+    if (win) {
+      setWinner(win);
+    }
+
+    setBoard([...board]);
+    setTurn(!turn);
+  }
   return (
     <div className="grid-wrapper">
-      <h1 className="grid-highlight"> Current turn : {(turn ? "O" : "X")}</h1>
+      {winner && (
+        <>
+          <h1 className="turn-highlight"> winner is {winner} </h1>
+          <button className="reset">Reset Game </button>
+        </>
+      )}
+
+      <h1 className="turn-highlight"> Current turn : {(turn ) ? "O" : "X"}</h1>
       <div className="grid">
         {board.map((el, idx) => (
-          <Card key={idx} />
+          <Card key={idx} onPlay={play} player={el} index={idx} />
         ))}
       </div>
     </div>
